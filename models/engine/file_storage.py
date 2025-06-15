a0fdee377f181d3e2e5dc3108ed38b03c9f501fe
Urom Jehoshaphat <jehoshaphaturom@gmail.com>
2025-06-15 19:45:15 +0100

    Add deletion and selective all

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,15 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return FileStorage.__objects
+        new_dict = {}
+        for key, value in FileStorage.__objects.items():
+            if type(value) == cls:
+                new_dict[key] = value
+        return new_dict
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -47,4 +53,14 @@ class FileStorage:
                 for key, val in temp.items():
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
+            pass
+
+    def delete(self, obj=None):
+        """ delete obj from __objects if it’s inside """
+        if obj is None:
+            return
+        try:
+            key = obj.to_dict()['__class__'] + '.' + obj.id
+            del FileStorage.__objects[key]
+        except KeyError:
             pass
