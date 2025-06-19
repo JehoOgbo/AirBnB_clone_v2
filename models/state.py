f240877a6e9fd88c85b2a3d96c2a1647f8bb3f8e
Urom Jehoshaphat <jehoshaphaturom@gmail.com>
2025-06-19 19:55:41 +0100

    work for both file and db

--- models/state.py
+++ models/state.py
@@ -3,17 +3,21 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.__init__ import env
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    state = relationship("City", backref='State')
+    if env == 'db':
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        state = relationship("City", backref='State')
 
-    @property
-    def cities(self):
-        """ returns the list of City instances with
-        state_id equals to the current State.id"""
-        from models.city import City
-        return len(storage.all(City))
+        @property
+        def cities(self):
+            """ returns the list of City instances with
+            state_id equals to the current State.id"""
+            from models.city import City
+            return len(storage.all(City))
+    else:
+        name = ""
