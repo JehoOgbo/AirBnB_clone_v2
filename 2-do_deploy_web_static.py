81a4b67ea52a33f8fbd8db60b0743bc6f6642f29
Urom Jehoshaphat <jehoshaphaturom@gmail.com>
2025-07-21 20:06:40 +0100

    do this

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,34 +1,30 @@
 #!/usr/bin/python3
 """
-Fabric script (based on the file 1-pack_web_static.py)
-that distributes an archive to your web servers,
-using the function do_deploy
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
 """
 
-
-from fabric.api import *
-import time
+from fabric.api import put, run, env
+from os.path import exists
 env.hosts = ['142.44.167.228', '144.217.246.195']
 
 
 def do_deploy(archive_path):
-    """function that distributes the archive""" 
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
+        return False
     try:
-        """ get the filename"""
-        """split using '/' and the last item is the filename"""
-        filename = archive_path.split("/")[-1]
-
-        """get the filename without the extension"""
-        no_ext = filename.split(".")[0]
-
-        """path is name of folder to uncompress the archive to"""
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
-
         put(archive_path, '/tmp/')
-        run("mkdir -p {}{}/".format(path, no_ext))
-        run("tar -xzf /tmp{} -C {}{}/".format(filename, path, no_ext))
-        run("rm /tmp/{}".format(archive_path))
-        run("rm /data/web_static/current")
-        run("ln -sf /data/web_static/releases/{} /data/web_static/current".format(no_ext)
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
     except Exception:
         return False
