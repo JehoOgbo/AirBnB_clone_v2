5c0916cfe69ea3b74c69c03c9b993f06206627b7
Urom Jehoshaphat <jehoshaphaturom@gmail.com>
2025-07-28 16:36:47 +0100

    remove unnecessary parenthesis

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -68,7 +68,7 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         Sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(sess_factory)
-        self.__session = Session()
+        self.__session = Session
 
     def close(self):
         """call remove method on self.__session)"""
