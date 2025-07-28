963ac39fe0d2ea7c20b4d5bb4f8341f39cf2ee07
Urom Jehoshaphat <jehoshaphaturom@gmail.com>
2025-07-28 15:46:59 +0100

    style validation

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -43,7 +43,7 @@ def num_template(n):
 
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def num_odd_or_even(n):
-    return render_template("6-number_odd_or_even.html", num = n)
+    return render_template("6-number_odd_or_even.html", num=n)
 
 
 if __name__ == '__main__':
