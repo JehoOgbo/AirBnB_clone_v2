8ff4784b341e630b4af112c427f20cc332e8f5ef
Urom Jehoshaphat <jehoshaphaturom@gmail.com>
2025-07-28 17:00:52 +0100

    make modification ' use state_id instead of id '

--- models/state.py
+++ models/state.py
@@ -30,6 +30,6 @@ else:
             city_list = []
             all_cities = storage.all(City)
             for city in all_cities.values():
-                if city.id == self.id:
+                if city.state_id == self.id:
                     city_list.append(city)
             return city_list
