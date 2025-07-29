789220f0e41ac27ef903fbb714dce2e1c8cd43f7
Urom Jehoshaphat <jehoshaphaturom@gmail.com>
2025-07-29 11:46:55 +0100

    try something

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,23 +1,49 @@
 #!/usr/bin/python3
-""" starts a web app for airbnb_clone """
+""" basic flask web app """
 from flask import Flask, render_template
-from models import storage
+from markupsafe import escape
 
 
 app = Flask(__name__)
 
 
-@app.teardown_app_context
-def teardown():
-    """call close method after every request"""
-    storage.close()
+@app.route("/", strict_slashes=False)
+def Hello():
+    return "Hello HBNB!"
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    """ list all states"""
-    list_state = storage.all('State')
-    return render_template("7-states_list.html", lister=list_state)
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    texter = escape(text).replace("_", " ")
+    return f"C {texter}"
+
+
+@app.route("/python/", defaults={"text": "is cool"},
+           strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_is_fun(text):
+    texter = escape(text).replace("_", " ")
+    return f"Python {texter}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def n_is_a_number(n):
+    return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def num_template(n):
+    return render_template("5-number.html", num=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def num_odd_or_even(n):
+    return render_template("6-number_odd_or_even.html", num=n)
 
 
 if __name__ == '__main__':
