f9056dae5d4ee47ab587d21d2e6e6b38fcc4f28b
Urom Jehoshaphat <jehoshaphaturom@gmail.com>
2025-07-29 11:54:52 +0100

    rearrange

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,18 +12,17 @@ from models import storage
 app = Flask(__name__)
 
 
-@app.teardown_app_context
-def teardown():
-    """call close method after every request"""
-    storage.close()
-
-
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """ list all states"""
     list_state = storage.all('State')
     return render_template("7-states_list.html", lister=list_state)
 
+
+@app.teardown_app_context
+def teardown():
+    """call close method after every request"""
+    storage.close()
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
