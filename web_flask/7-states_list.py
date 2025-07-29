b428002b86c8d0119d4a70213a76baac181fbe4d
Urom Jehoshaphat <jehoshaphaturom@gmail.com>
2025-07-29 11:37:21 +0100

    Frontend backend integration

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""starts a web app for airbnb_clone"""
+from flask import Flask
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.teardown_app_context
+def teardown():
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    list_state = storage.all('State')
+    return render_template("7-states_list.html", lister=list_state)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=5000)
