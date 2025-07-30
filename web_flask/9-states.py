f9019140db8fe253d4b4ea7d02bfcd37bbb3d7b8
Urom Jehoshaphat <jehoshaphaturom@gmail.com>
2025-07-30 18:06:25 +0100

    change behaviour with empty db

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,45 +1,28 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states_list: HTML page with a list of all State objects in DBStorage.
 """
-from models import storage
-from flask import Flask
-from flask import render_template
+starts a Flask web application
+"""
 
+from flask import Flask, render_template
+from models import *
+from models import storage
 app = Flask(__name__)
 
 
-@app.route("/states", strict_slashes=False)
-def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
-
-    States are sorted by name.
-    """
-    states = storage.all("State").values()
-    return render_template("9-states.html", states=states)
-
-
-@app.route("/states/<id>", strict_slashes=False)
-def states_id(id):
-    """Displays an HTML page with a list of cities
-    linked to a state
-    """
-    states = storage.all("State").values()
-    for state in states:
-        if state.id == id:
-            new = state.cities
-            return render_template("9-states.html", cities=new, static=state)
-    return render_template("9-states.html")
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<state_id>', strict_slashes=False)
+def states(state_id=None):
+    """display the states and cities listed in alphabetical order"""
+    states = storage.all("State")
+    if state_id is not None:
+        state_id = 'State.' + state_id
+    return render_template('9-states.html', states=states, state_id=state_id)
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
